8aa02d9a0f76aeb348a0f3cc30254205cceb9bb7
曾璐 <865514198@qq.com>
2018-07-10 14:02:51 +0800

    架子

---
+++ MVC AutoLogin/Startup.cs
@@ -0,0 +1,146 @@
+﻿using IdentityModel.Client;
+using IdentityServer3.Core;
+using IdentityServer3.Core.Configuration;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using MVC_AutoLogin.IdentityServer;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Helpers;
+
+[assembly: OwinStartup(typeof(MVC_AutoLogin.Startup))]
+namespace MVC_AutoLogin
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // todo: replace with serilog
+            //LogProvider.SetCurrentLogProvider(new DiagnosticsTraceLogProvider());
+
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = Constants.ClaimTypes.Subject;
+            JwtSecurityTokenHandler.InboundClaimTypeMap = new Dictionary<string, string>();
+
+            app.Map("/identity", idsrvApp =>
+            {
+                idsrvApp.UseIdentityServer(new IdentityServerOptions
+                {
+                    SiteName = "Embedded IdentityServer",
+                    SigningCertificate = LoadCertificate(),
+
+                    Factory = new IdentityServerServiceFactory()
+                                .UseInMemoryUsers(Users.Get())
+                                .UseInMemoryClients(Clients.Get())
+                                .UseInMemoryScopes(Scopes.Get()),
+
+                    AuthenticationOptions = new IdentityServer3.Core.Configuration.AuthenticationOptions
+                    {
+                        EnablePostSignOutAutoRedirect = true,
+                        IdentityProviders = ConfigureIdentityProviders
+                    }
+                });
+            });
+
+            app.UseResourceAuthorization(new AuthorizationManager());
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "Cookies"
+            });
+
+
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+            {
+                Authority = "https://localhost:44319/identity",
+                //ClientId唯一
+                ClientId = "mvc2",
+                Scope = "openid profile roles sampleApi",
+                ResponseType = "id_token token",
+                //设置客户端
+                RedirectUri = "https://localhost:44368/",
+
+                SignInAsAuthenticationType = "Cookies",
+                UseTokenLifetime = false,
+
+                Notifications = new OpenIdConnectAuthenticationNotifications
+                {
+                    SecurityTokenValidated = async n =>
+                    {
+                        var nid = new ClaimsIdentity(
+                            n.AuthenticationTicket.Identity.AuthenticationType,
+                            Constants.ClaimTypes.GivenName,
+                            Constants.ClaimTypes.Role);
+
+                        // get userinfo data
+                        var userInfoClient = new UserInfoClient(
+                            new Uri(n.Options.Authority + "/connect/userinfo"),
+                            n.ProtocolMessage.AccessToken);
+
+                        var userInfo = await userInfoClient.GetAsync();
+                        userInfo.Claims.ToList().ForEach(ui => nid.AddClaim(new Claim(ui.Item1, ui.Item2)));
+
+                        // keep the id_token for logout
+                        nid.AddClaim(new Claim("id_token", n.ProtocolMessage.IdToken));
+
+                        // add access token for sample API
+                        nid.AddClaim(new Claim("access_token", n.ProtocolMessage.AccessToken));
+
+                        // keep track of access token expiration
+                        nid.AddClaim(new Claim("expires_at", DateTimeOffset.Now.AddSeconds(int.Parse(n.ProtocolMessage.ExpiresIn)).ToString()));
+
+                        // add some other app specific claim
+                        nid.AddClaim(new Claim("app_specific", "some data"));
+
+                        n.AuthenticationTicket = new AuthenticationTicket(
+                            nid,
+                            n.AuthenticationTicket.Properties);
+                    },
+
+                    RedirectToIdentityProvider = n =>
+                    {
+                        if (n.ProtocolMessage.RequestType == OpenIdConnectRequestType.LogoutRequest)
+                        {
+                            var idTokenHint = n.OwinContext.Authentication.User.FindFirst("id_token");
+
+                            if (idTokenHint != null)
+                            {
+                                n.ProtocolMessage.IdTokenHint = idTokenHint.Value;
+                            }
+                        }
+
+                        return Task.FromResult(0);
+                    }
+                }
+            });
+        }
+
+        private void ConfigureIdentityProviders(IAppBuilder app, string signInAsType)
+        {
+            //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions
+            //{
+            //    AuthenticationType = "Google",
+            //    Caption = "Sign-in with Google",
+            //    SignInAsAuthenticationType = signInAsType,
+
+            //    ClientId = "701386055558-9epl93fgsjfmdn14frqvaq2r9i44qgaa.apps.googleusercontent.com",
+            //    ClientSecret = "3pyawKDWaXwsPuRDL7LtKm_o"
+            //});
+        }
+
+        X509Certificate2 LoadCertificate()
+        {
+            return new X509Certificate2(
+                string.Format(@"{0}\bin\identityServer\idsrv3test.pfx", AppDomain.CurrentDomain.BaseDirectory), "idsrv3test");
+        }
+    }
+}
